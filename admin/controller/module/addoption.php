75366a7068766398b08af6d2a90f1d821edd359b
manishmittal <manish@opensource>
2016-09-09 12:17:15 +0530

    first commit

---
+++ admin/controller/module/addoption.php
@@ -0,0 +1,83 @@
+<?php
+/*
+ * @support
+ * http://www.opensourcetechnologies.com/contactus.html
+ * sales@opensourcetechnologies.com
+* */
+class ControllerModuleAddoption extends Controller {
+	private $error = array();
+
+	public function index() {
+		$this->load->language('module/addoption');
+
+		$this->document->setTitle($this->language->get('heading_title'));
+
+		$this->load->model('setting/setting');
+
+		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+			$this->model_setting_setting->editSetting('addoption', $this->request->post);
+
+			$this->session->data['success'] = $this->language->get('text_success');
+
+			$this->response->redirect($this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL'));
+		}
+
+		$data['heading_title'] = $this->language->get('heading_title');
+
+		$data['text_edit'] = $this->language->get('text_edit');
+		$data['text_enabled'] = $this->language->get('text_enabled');
+		$data['text_disabled'] = $this->language->get('text_disabled');
+
+		$data['entry_status'] = $this->language->get('entry_status');
+
+		$data['button_save'] = $this->language->get('button_save');
+		$data['button_cancel'] = $this->language->get('button_cancel');
+
+		if (isset($this->error['warning'])) {
+			$data['error_warning'] = $this->error['warning'];
+		} else {
+			$data['error_warning'] = '';
+		}
+
+		$data['breadcrumbs'] = array();
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/dashboard', 'token=' . $this->session->data['token'], 'SSL')
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_module'),
+			'href' => $this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL')
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('heading_title'),
+			'href' => $this->url->link('module/addoption', 'token=' . $this->session->data['token'], 'SSL')
+		);
+
+		$data['action'] = $this->url->link('module/addoption', 'token=' . $this->session->data['token'], 'SSL');
+
+		$data['cancel'] = $this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL');
+
+		if (isset($this->request->post['addoption_status'])) {
+			$data['addoption_status'] = $this->request->post['addoption_status'];
+		} else {
+			$data['addoption_status'] = $this->config->get('addoption_status');
+		}
+
+		$data['header'] = $this->load->controller('common/header');
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['footer'] = $this->load->controller('common/footer');
+
+		$this->response->setOutput($this->load->view('module/addoption.tpl', $data));
+	}
+
+	protected function validate() {
+		if (!$this->user->hasPermission('modify', 'module/addoption')) {
+			$this->error['warning'] = $this->language->get('error_permission');
+		}
+
+		return !$this->error;
+	}
+}
---
+++ admin/language/english/module/.svn/text-base/addoption.php.svn-base
@@ -0,0 +1,18 @@
+<?php
+/*
+ * @support
+ * http://www.opensourcetechnologies.com/contactus.html
+ * sales@opensourcetechnologies.com
+* */
+// Heading
+$_['heading_title']    = 'Add Options';
+
+$_['text_module']      = 'Modules';
+$_['text_success']     = 'Success: You have modified add option module!';
+$_['text_edit']        = 'Edit Addoption Module';
+
+// Entry
+$_['entry_status']     = 'Status';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify addoption module!';
